793f5097b8e80b90f599210b4f79503e8003c919
Marco Antonio Felix Torrez <159277886+MarcoFelix13@users.noreply.github.com>
2024-04-09 14:55:18 +0000

    Clase Diferencia edades

---
+++ difEdades/Principal.java
@@ -0,0 +1,9 @@
+package difEdades;
+
+public class Principal {
+    public static void main(String[] args) {
+        DifEdades Dedad = new DifEdades();
+        Dedad.difEdades();
+
+    }
+}
--- ine/Ine.java
+++ ine/Ine.java
@@ -1,29 +1,35 @@
 package ine;
-
+// si un mexicano puede o no tramitar su credencial de elector
+// Autor: Marco Felix
+// Fecha: 19 de marzo 2024
 import java.util.Scanner;
-
-public class Ine {
+public class Ine{
     public void determinarSiIne(){
         //definir un objeto Scanner
         Scanner teclado = new Scanner(System.in);
         //definir variable de salida
         String resultado;
         //definir variable de entrada
-        int anioActaul,anioNacimiento;
-        //definir variable de auxiliar
+        int anioActual,anioNacimiento;
+        //definir variable auxiliar
         int anios;
 
-        System.out.print("Propmociona año actual");
+        System.out.print("Proporciona Año Actual: ");
         anioActual = teclado.nextInt();
-        System.out.print("Propmociona año de Nacimiento");
+        System.out.print("Proporciona Año de Nacimiento:");
         anioNacimiento = teclado.nextInt();
 
-        anios = anioActaul - anioNacimiento;
+        anios = anioActual - anioNacimiento;
 
         if(anios>=18)
             resultado = "Usted puede tramitar su INE";
+        
         else
-            resultado = "Usted NO PUEDE tramitar su INE";
+            resultado = "Usted NO PUEDS tramitar su INE";
+        
+        System.out.println(resultado);
+        
+
 
     }
 
