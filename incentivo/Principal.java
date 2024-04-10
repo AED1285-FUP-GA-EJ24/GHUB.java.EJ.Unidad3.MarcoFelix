4e87662a96c36cbe6c59a942cade759a79ead33d
Marco Antonio Felix Torrez <159277886+MarcoFelix13@users.noreply.github.com>
2024-04-10 14:47:51 +0000

    Clase incentivo

---
+++ incentivo/Principal.java
@@ -0,0 +1,9 @@
+package incentivo;
+
+public class Principal {
+    public static void main(String[] args) {
+        Incentivo incentivo = new Incentivo();
+        incentivo.incentivo();
+
+    }
+}
